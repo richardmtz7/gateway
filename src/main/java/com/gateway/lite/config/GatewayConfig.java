4445e601a1e61cc9733644e9d0d7e29abd4c75eb
richardmtz7 <richardmtz710@gmail.com>
2024-09-14 04:03:36 -0500

    Add condig cors

--- src/main/java/com/gateway/lite/config/GatewayConfig.java
+++ src/main/java/com/gateway/lite/config/GatewayConfig.java
@@ -5,15 +5,20 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class GatewayConfig {
+public class GatewayConfig implements WebMvcConfigurer {
 	
 	@Value("${BUSINESS_SERVICE_URI}")
     private String businessServiceUri;
 
     @Value("${AUTH_SERVICE_URI}")
     private String authServiceUri;
+    
+    @Value("${url.front}")
+	private String urlFront;
     
 	@Bean
 	public RouteLocator  customRouteLocator(RouteLocatorBuilder builder) {
@@ -24,4 +29,13 @@ public class GatewayConfig {
 	                .uri(authServiceUri))
 	            .build();
 	}
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(urlFront)
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
 }
